414489407860c2dbc14453ed4960dc383240cec1
Luan Kenzley <kenzleydev@outlook.com>
2023-05-01 19:47:12 -0300

    subindo estruturas iniciais

---
+++ src/test/java/com/luankenzley/soap/webservice/custumersadministration/CustumersAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luankenzley.soap.webservice.custumersadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustumersAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
